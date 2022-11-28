3879a54b239f6c93f3d993d14719ab98bb96d12c
Igor Kazel <igor.joer@gmail.com>
2022-11-28 21:00:21 +0300

    Program added

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] array, string message)
+{
+    Console.WriteLine(message);
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
+
+string[] ChangeArray(string[] array)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            j++;
+        }
+    }
+    string[] newArray = new string[j];
+    j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+
+string[] array = { "123", "1234", "qwerty", "qwe", ":)" };
+PrintArray(array, "Первоначальный массив:");
+array = ChangeArray(array);
+PrintArray(array, "Изменённый массив:");
